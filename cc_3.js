0bca45c2871b2c3ca28709b63b648189b1141a89
Rachael <rachy.torok@gmail.com>
2025-01-30 15:50:32 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,6 @@
+// Task 1 - Product Price Management
+
+let prices = [10, 20, 30, 40, 50]; // array of prices
+prices.push(60);//add new price to array
+prices.pop();//remove last price from array
+console.log("Updated Prices:", prices);//print array
